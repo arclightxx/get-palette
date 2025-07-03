be19ee1e2bfa23198d6602bc47a3ac297c7bcbe5
arc <zaveyborodaid@gmail.com>
2025-07-03 20:48:58 +0500

    Add config file

--- config.go
+++ config.go
@@ -1,13 +1,25 @@
 package main
 
+import "flag"
+
 const (
 	minW        = 32
 	maxW        = 1980
-	minCellSize = 1
 )
 
 type Config struct {
-	FilePath string
-	Scale    int // Scale < 0 to shrink the image; Scale > 0 to grow the image
-	CellSize int
+	inputPath     string
+	outoutPath    string
+	cellSize      int
+	gridThickness int
+}
+
+func NewConfig() *Config {
+	c := &Config{}
+	flag.StringVar(&c.inputPath, "input", "./img", "Path to input file")
+	flag.StringVar(&c.outoutPath, "output", "./out", "Path to output file")
+	flag.IntVar(&c.cellSize, "cellsize", 1, "Target cell size in pixels")
+	flag.IntVar(&c.gridThickness, "grid", 1, "Grid line thickness")
+
+	return c
 }
