3cf8b2f8ff56b3c82b7bbb90f27fde7d1fd5f1b3
arc <zaveyborodaid@gmail.com>
2025-07-03 22:23:29 +0500

    Refactor -h output

--- config.go
+++ config.go
@@ -3,21 +3,23 @@ package main
 import "flag"
 
 const (
-	minW        = 32
-	maxW        = 1980
+	minW = 32
+	maxW = 1980
 )
 
 type Config struct {
 	inputPath     string
 	outoutPath    string
+	scale         int
 	cellSize      int
 	gridThickness int
 }
 
 func NewConfig() *Config {
 	c := &Config{}
-	flag.StringVar(&c.inputPath, "input", "./img", "Path to input file")
-	flag.StringVar(&c.outoutPath, "output", "./out", "Path to output file")
+	flag.StringVar(&c.inputPath, "input", "", "Path to input file")
+	flag.StringVar(&c.outoutPath, "output", "", "Path to output file")
+	flag.IntVar(&c.scale, "scale", 0, "Scale < 0 to shrink the image\nScale > 0 to grow the image")
 	flag.IntVar(&c.cellSize, "cellsize", 1, "Target cell size in pixels")
 	flag.IntVar(&c.gridThickness, "grid", 1, "Grid line thickness")
 
