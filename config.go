accdcd93e14384c88374926f2eb8ecf8181fa66e
arc <zaveyborodaid@gmail.com>
2025-07-04 18:10:06 +0500

    Change cellSize default value

--- config.go
+++ config.go
@@ -20,7 +20,7 @@ func NewConfig() *Config {
 	flag.StringVar(&c.inputPath, "input", "", "Path to input file")
 	flag.StringVar(&c.outoutPath, "output", "", "Path to output file")
 	flag.IntVar(&c.scale, "scale", 0, "Scale < 0 to shrink the image\nScale > 0 to grow the image")
-	flag.IntVar(&c.cellSize, "cellsize", 1, "Target cell size in pixels")
+	flag.IntVar(&c.cellSize, "cellsize", 2, "Target cell size in pixels")
 	flag.IntVar(&c.gridThickness, "grid", 1, "Grid line thickness")
 
 	return c
