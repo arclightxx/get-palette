2f237211a8c8854d2377f3b09ba389c8075ed346
arc <zaveyborodaid@gmail.com>
2025-07-11 20:19:18 +0500

    Fix typo

--- config.go
+++ config.go
@@ -9,7 +9,7 @@ const (
 
 type Config struct {
 	inputPath     string
-	outoutPath    string
+	outputPath    string
 	scale         int
 	cellSize      int
 	gridThickness int
@@ -18,7 +18,7 @@ type Config struct {
 func NewConfig() *Config {
 	c := &Config{}
 	flag.StringVar(&c.inputPath, "input", "", "Path to input file")
-	flag.StringVar(&c.outoutPath, "output", "", "Path to output file")
+	flag.StringVar(&c.outputPath, "output", "", "Path to output file")
 	flag.IntVar(&c.scale, "scale", 0, "Scale < 0 to shrink the image\nScale > 0 to grow the image")
 	flag.IntVar(&c.cellSize, "cellsize", 2, "Target cell size in pixels")
 	flag.IntVar(&c.gridThickness, "grid", 1, "Grid line thickness")
